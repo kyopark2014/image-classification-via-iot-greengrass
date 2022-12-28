55a3ace6618cd01caf9d9afb01d1eecf05d56cd6
ksdyb.park <ksdyb@amazon.com>
2022-12-28 21:22:57 +0900

    update

--- dlr-inference/inference.py
+++ dlr-inference/inference.py
@@ -5,24 +5,33 @@ from dlr import DLRModel
 from numpy import argsort, frombuffer, fromstring, load, uint8
 from os import environ, path
 from ast import literal_eval
+import os
 
 logger = getLogger()
 handler = StreamHandler(stdout)
 logger.setLevel(INFO)
 logger.addHandler(handler)
 
-IMAGE_DIR = './images'
+IMAGE_DIR = f'{os.getcwd()}/images'
+print('IMAGE_DIR:', IMAGE_DIR)
+
 SCORE_THRESHOLD = 0.3
 MAX_NO_OF_RESULTS = 5
 SHAPE = (224, 224)
 
+MODEL_DIR = f'{os.getcwd()}/model'
+print('MODEL_DIR:', MODEL_DIR)
+
 # Read synset file
-MODEL_DIR = './model'
 LABELS = path.join(MODEL_DIR, "synset.txt")
 with open(LABELS, "r") as f:
     synset = literal_eval(f.read())
 
-dlr_model = DLRModel(MODEL_DIR, 'cpu')
+def load_model(model_dir):
+    #model = DLRModel(model_dir, 'cpu')
+    model = DLRModel(model_dir, dev_type='gpu', use_default_dlr=False)
+    print('MODEL was loaded')
+    return model
 
 def load_image(image_path):
     # Case insenstive check of the image type.
@@ -58,10 +67,10 @@ def load_image(image_path):
         exit(1)
     return image_data
 
-def predict_from_image(image_data):
+def predict_from_image(model, image_data):
     try:
         # Run DLR to perform inference with DLC optimized model
-        model_output = dlr_model.run(image_data)
+        model_output = model.run(image_data)
 
         probabilities = model_output[0][0]
         sort_classes_by_probability = argsort(probabilities)[::-1]
@@ -73,11 +82,13 @@ def predict_from_image(image_data):
         logger.error("Exception occured during prediction: {}".format(e))
 
 def main():
+    model = load_model(MODEL_DIR)
+
     image = load_image(path.join(IMAGE_DIR, 'cat.jpeg'))
     cvimage = resize(image, SHAPE)
 
     if cvimage is not None:
-        predict_from_image(cvimage)
+        predict_from_image(model, cvimage)
         return
     else:
         logger.error("Unable to capture an image using camera")
