70e8332bfa68a30008ba54eac01ef209e85a2213
ksdyb.park <ksdyb@amazon.com>
2023-01-01 22:38:51 +0900

    update

--- dev/local/inference-test.py
+++ dev/local/inference-test.py
@@ -15,8 +15,7 @@ logger.setLevel(INFO)
 #logging_handler = StreamHandler(stdout)
 #logger.addHandler(logging_handler)
 
-#IMAGE_DIR = f'{os.getcwd()}/images'
-IMAGE_DIR = '/greengrass/v2/packages/artifacts/com.custom.ImageClassifier/1.0.0/images'
+IMAGE_DIR = f'{os.getcwd()}/images'
 
 print('IMAGE_DIR:', IMAGE_DIR)
 
